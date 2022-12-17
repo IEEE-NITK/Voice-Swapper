3f9108a9f422fa7ccd75f82d53260294d0e4fa19
harry7337 <75776208+harry7337@users.noreply.github.com>
2022-12-17 14:25:42 +0530

    Added tanh and removed DoubleConv,OutConv
    Removed the initial double convolution and also removed the convolution at the end(outconv)  
    Also added tanh layer at the end  
    Changed the input arguments for the encode and decode layers

--- unet_generator.py
+++ unet_generator.py
@@ -8,20 +8,21 @@ class Generator(nn.Module):
     self.out_channels = out_channels
     self.bilinear = bilinear
 
-    self.inc = (DoubleConv(in_channels, 64))
-    self.encode1 = (Encode(64, 128))
-    self.encode2 = (Encode(128, 256))
-    self.encode3 = (Encode(256, 512))
+    # self.inc = (DoubleConv(in_channels, 64))
+    self.encode1 = (Encode(in_channels, 64))
+    self.encode2 = (Encode(64, 128))
+    self.encode3 = (Encode(128, 256))
     factor = 2 if bilinear else 1
-    self.encode4 = (Encode(512, 1024 // factor))
-    self.decode1 = (Decode(1024, 512 // factor, bilinear))
-    self.decode2 = (Decode(512, 256 // factor, bilinear))
-    self.decode3 = (Decode(256, 128 // factor, bilinear))
-    self.decode4 = (Decode(128, 64, bilinear))
-    self.outc = (OutConv(64, out_channels))
+    self.encode4 = (Encode(256, 512 // factor))
+    self.decode1 = (Decode(512, 256 // factor, bilinear))
+    self.decode2 = (Decode(256, 128 // factor, bilinear))
+    self.decode3 = (Decode(128, 64 // factor, bilinear))
+    self.decode4 = (Decode(64, out_channels, bilinear))
+    self.out = nn.Tanh()
+    # self.outc = (OutConv(64, out_channels))
   
   def forward(self, x):
-    x1 = self.inc(x)
+    x1 = x
     x2 = self.encode1(x1)
     x3 = self.encode2(x2)
     x4 = self.encode3(x3)
@@ -30,5 +31,5 @@ class Generator(nn.Module):
     x = self.decode2(x, x3)
     x = self.decode3(x, x2)
     x = self.decode4(x, x1)
-    logits = self.outc(x)
-    return logits
+    output = self.outc(x)
+    return output
